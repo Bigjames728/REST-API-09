be1e9b34e492973fc713c502af0fd85eb579e3eb
Bigjames728 <bigjames728@gmail.com>
2021-06-14 14:29:42 -0700

    required routes in app.js

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const express = require('express');
 const morgan = require('morgan');
 const { sequelize, User, Course } = require('./models');
 
+const routes = require('./routes/userRoutes');
+
+
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
 // create the Express app
 const app = express();
+
+app.use('/api', routes);
+app.use(express.json());
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
