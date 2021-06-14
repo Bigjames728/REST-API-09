4ecec5e60b0d0f886ca498a0f85a9920aaff877b
Bigjames728 <bigjames728@gmail.com>
2021-06-14 14:56:11 -0700

    Added a GET and POST route to userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,27 @@ const router = express.Router();
 const User = require('../models').User;
 const { asyncHandler } = require('../middleware/async-handler');
 
+// GET route that returns the currently authenticated user along with a 200 status code
 router.get('/api/users', asyncHandler( async (req, res) => {
-
+    const user = req.currentUser;
+    res.status(200).json(user);
 }));
 
-
-
-
+// Route that creates a new user.
+router.post('/users', asyncHandler(async (req, res) => {
+    try {
+      await User.create(req.body);
+      res.status(201).json({ "message": "Account successfully created!" });
+    } catch (error) {
+      if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+        const errors = error.errors.map(err => err.message);
+        res.status(400).json({ errors });   
+      } else {
+        throw error;
+      }
+    }
+}));
+  
+  
 
 module.exports = router;
