8663d2aa3d97475f539826c787fa969c0abee574
Bigjames728 <bigjames728@gmail.com>
2021-06-14 14:48:07 -0700

    required asynchandler in userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const User = require('../models').User;
+const { asyncHandler } = require('../middleware/async-handler');
 
+router.get('/api/users', asyncHandler( async (req, res) => {
 
+}));
 
 
 
