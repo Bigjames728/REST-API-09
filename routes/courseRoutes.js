603040f0bbb18ecc7da0c8c3c3b3fb685388fbc8
Bigjames728 <bigjames728@gmail.com>
2021-06-15 12:01:35 -0700

    Added in all routes needed as well as some comments.

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 const Course = require('../models').Course;
 const User = require('../models').User;
@@ -17,7 +16,7 @@ router.get('/', asyncHandler( async (req, res) => {
     }
 }));
 
-// GET route that will return the corresponding course along with the User that owns that course and a 200 HTTP status code.
+// GET route that will return a specific course along with the User that owns that course and a 200 HTTP status code.
 router.get('/:id', asyncHandler ( async (req, res) => {
     const course = await Course.findByPk(req.params.id, {
         include: [{ model: User }]
@@ -33,7 +32,7 @@ router.get('/:id', asyncHandler ( async (req, res) => {
 router.post('/', asyncHandler (async (req, res) => {
     try {
         await Course.create(req.body);
-        res.status(201).location('/');
+        res.status(201).location('/'); // I think I need some work here. I think I need to add the id to the end of this url when the course is created.
     } catch (error) {
         if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
             const errors = error.errors.map(err => err.message);
@@ -44,8 +43,37 @@ router.post('/', asyncHandler (async (req, res) => {
     }
 }));
 
+// PUT route that will update the corresponding course and return a 204 status code and no content.
+router.put('/:id', asyncHandler( async (req, res) => {
+    let course;
+    try {
+        course = await Course.findByPk(req.params.id);
+        if (course) {
+        await course.update(req.body);
+        res.status(204).end();
+        } else {
+        res.status(404).json({ message: "We weren't able to update this course at this time." });
+        }
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });   
+        } else {
+            throw error;
+        }
+    } 
+}));
 
-
+// DELETE route that will delete a specific course and return a 204 HTTP status code and no content.
+router.delete('/:id', asyncHandler( async(req, res) => {
+    const course = await Course.findByPk(req.params.id);
+    if (course) {
+        await course.destroy();
+        res.status(204).end();
+    } else {
+        res.status(404).json({ message: "Couldn't delete this course at this time." });
+    }
+}));
 
 
 module.exports = router;
