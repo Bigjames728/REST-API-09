3771437da9775e339a328a2dbbf98cc19b10887f
Bigjames728 <bigjames728@gmail.com>
2021-06-14 22:37:53 -0700

    added a GET route to courseRoutes to get all course info and display in json

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 const Course = require('../models').Course;
+const User = require('../models').User;
+const { asyncHandler } = require('../middleware/async-handler');
 
-
+// GET route that will return a list of all courses including the User that owns each course and a 200 HTTP status code.
+router.get('/', asyncHandler( async (req, res) => {
+    const course = await Course.findAll({
+        include: [{ model: User }]
+    });
+    if (course) {
+        res.status(200).json(course)
+    } else {
+        res.status(404).json({ message: "No courses were found." })
+    }
+}));
 
 
 
