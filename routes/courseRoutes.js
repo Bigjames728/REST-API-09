3c2a8df39f9c9d40a45358b6037186f33d368b4e
Bigjames728 <bigjames728@gmail.com>
2021-06-15 11:36:33 -0700

    Added a GET route for specific course and a POST route to create a course

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -17,6 +17,33 @@ router.get('/', asyncHandler( async (req, res) => {
     }
 }));
 
+// GET route that will return the corresponding course along with the User that owns that course and a 200 HTTP status code.
+router.get('/:id', asyncHandler ( async (req, res) => {
+    const course = await Course.findByPk(req.params.id, {
+        include: [{ model: User }]
+    });
+    if (course) {
+        res.status(200).json(course);
+    } else {
+        res.status(404).json({ message: "No course was found." })
+    }
+}));
+
+// POST route that will create a new course, set the Location header to the URI for the newly created course, and return a 201 HTTP status code and no content.
+router.post('/', asyncHandler (async (req, res) => {
+    try {
+        await Course.create(req.body);
+        res.status(201).location('/');
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });   
+          } else {
+            throw error;
+          }
+    }
+}));
+
 
 
 
